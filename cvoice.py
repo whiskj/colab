4c5ff5e0c66d2a68d25a8fa0a339cdd779d05cd1
whiskj <wkhsisks66@gmail.com>
2025-07-01 20:28:51 +0100

    pushing fixes

--- cvoice.py
+++ cvoice.py
@@ -10,14 +10,14 @@ cosyvoice = CosyVoice('/home/system/CosyVoice/pretrained_models/CosyVoice-300M-2
 # NOTE if you want to reproduce the results on https://funaudiollm.github.io/cosyvoice2, please add text_frontend=False during inference
 # zero_shot usage
 prompt_speech_16k = load_wav('girl.wav', 16000)
-
+"""
 for i, j in enumerate(cosyvoice.inference_zero_shot('Hey babe how are you doing, so can you like umm... help me write a birthday message for my sister? I\'m terrible with words and want to say something meaningful. but i keep drawing a blank.', 'slowly', prompt_speech_16k, stream=False)):
     torchaudio.save('cvoice_zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
 
 """
 for i, j in enumerate(cosyvoice.inference_cross_lingual('Hey babe how are you doing, so can you like umm... help me write a birthday message for my sister? I\'m terrible with words and want to say something meaningful. but i keep drawing a blank.', prompt_speech_16k, stream=False)):
     torchaudio.save('cvoice_cross_lingual_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
-
+"""
 source_speech_16k = load_wav('girl.wav', 16000)
 for i, j in enumerate(cosyvoice.inference_vc(source_speech_16k, prompt_speech_16k, stream=False)):
     torchaudio.save('cvoice_vc_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
