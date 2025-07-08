1c37cbdbaaac69a7180f1cddfdeb6e471e61b1c1
whiskj <wkhsisks66@gmail.com>
2025-07-08 20:58:23 +0100

    added fixes

--- vcraft.py
+++ vcraft.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('/home/system/VoiceCraft')
 import os
-#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-#os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["USER"] = "system"
 
 import torch
